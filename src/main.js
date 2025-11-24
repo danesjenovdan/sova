b36b11d94165ba6c60849c7e40fb6c610a27f571
LepkoQQ <lepko.san@gmail.com>
2025-11-24 16:17:37 +0100

    fix email buttons and selections

--- src/main.js
+++ src/main.js
@@ -326,6 +326,19 @@ function loadResultValues() {
   }
 }
 
+function selectEmailContent() {
+  const emailContent = document.querySelector(".js-email-content");
+  if (emailContent) {
+    const range = document.createRange();
+    range.selectNodeContents(emailContent);
+    const selection = window.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
+    return selection;
+  }
+  return null;
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   const checkBtn = document.querySelector(".js-check-btn");
   if (checkBtn) {
@@ -340,6 +353,47 @@ document.addEventListener("DOMContentLoaded", () => {
   if (resultsBtn) {
     resultsBtn.addEventListener("click", async () => {
       await nextContentPage();
+    });
+  }
+
+  const copyMailBtn = document.querySelector(".js-copy-mail-btn");
+  if (copyMailBtn) {
+    copyMailBtn.addEventListener("click", () => {
+      const emailContent = document.querySelector(".js-email-content");
+      if (emailContent) {
+        const selection = selectEmailContent();
+        try {
+          document.execCommand("copy");
+          selection.removeAllRanges();
+          const previousText = copyMailBtn.querySelector("span").textContent;
+          copyMailBtn.querySelector("span").innerHTML =
+            "<strong>SKOPIRANO!</strong>";
+          setTimeout(() => {
+            copyMailBtn.querySelector("span").textContent = previousText;
+          }, 2000);
+        } catch (err) {
+          console.error("Failed to copy text: ", err);
+        }
+      }
+    });
+  }
+
+  const openMailBtn = document.querySelector(".js-open-mail-btn");
+  if (openMailBtn) {
+    openMailBtn.addEventListener("click", () => {
+      const selection = selectEmailContent();
+      const emailContent = selection.toString().trim();
+      selection.removeAllRanges();
+      const emailSubject = document.querySelector(".js-email-subject");
+      const emailTo = document.querySelector(".js-email-to");
+      if (emailContent && emailSubject && emailTo) {
+        const mailtoLink = `mailto:${encodeURIComponent(
+          emailTo.textContent.trim()
+        )}?subject=${encodeURIComponent(
+          emailSubject.textContent.trim()
+        )}&body=${encodeURIComponent(emailContent)}`;
+        window.open(mailtoLink, "_blank");
+      }
     });
   }
 
